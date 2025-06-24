1188b83e5a4e9b342415a5056db9b8794aa438ef
NeaJuank <juankiudc@gmail.com>
2025-06-24 14:02:18 -0500

    Chatbot

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Navbar from "@components/navbar";
 import About from "@features/landingPage/sections/about";
 import Footer from "@features/landingPage/sections/footer";
@@ -5,9 +6,47 @@ import Hero from "@features/landingPage/sections/heroSection";
 import Reviews from "@features/landingPage/sections/review";
 import Services from "@features/landingPage/sections/services";
 import WhyChooseUs from "@features/landingPage/sections/whyChooseUs";
-import CarCard from '@features/landingPage/sections/cars'
+import CarCard from '@features/landingPage/sections/cars';
 
 const LandingPage = () => {
+
+  useEffect(() => {
+    if (
+      !window.chatbase ||
+      (typeof window.chatbase === 'function' &&
+        window.chatbase('getState') !== 'initialized')
+    ) {
+      window.chatbase = (...args: any[]) => {
+        if (!window.chatbase.q) {
+          window.chatbase.q = [];
+        }
+        window.chatbase.q.push(args);
+      };
+      window.chatbase = new Proxy(window.chatbase, {
+        get(target, prop) {
+          if (prop === 'q') {
+            return target.q;
+          }
+          return (...args) => target(prop, ...args);
+        },
+      });
+    }
+
+    const onLoad = () => {
+      const script = document.createElement('script');
+      script.src = 'https://www.chatbase.co/embed.min.js';
+      script.id = 'S4Z4Bmgf643TvxMcvfu8e';
+      script.setAttribute('domain', 'www.chatbase.co');
+      document.body.appendChild(script);
+    };
+
+    if (document.readyState === 'complete') {
+      onLoad();
+    } else {
+      window.addEventListener('load', onLoad);
+    }
+  }, []);
+
   return (
     <div>
       <header className="sticky top-0 z-100">
